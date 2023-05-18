d016d5a8fd364415dcea4e200a1b044ffe93f2bc
glax <--local>
2023-05-18 15:47:05 +0200

    Added more fields to Publication

--- Tranga/Publication.cs
+++ Tranga/Publication.cs
@@ -3,18 +3,30 @@
 public struct Publication
 {
     public string sortName { get; }
-    public string[] titles { get;  }
-    public string description { get; }
+    public string[,] altTitles { get;  }
+    public string? description { get; }
     public string[] tags { get; }
     public string posterUrl { get; } //maybe there is a better way?
+    public string[,] links { get; }
+    public int? year { get; }
+    public string? originalLanguage { get; }
+    public string status { get; }
     
+    public Connector connector { get; }
+    public string downloadUrl { get; }
 
-    public Publication(string sortName, string description, string[] titles, string[] tags, string posterUrl)
+    public Publication(string sortName, string? description, string[,] altTitles, string[] tags, string posterUrl, string[,] links, int? year, string? originalLanguage, string status, Connector connector, string downloadUrl)
     {
         this.sortName = sortName;
         this.description = description;
-        this.titles = titles;
+        this.altTitles = altTitles;
         this.tags = tags;
         this.posterUrl = posterUrl;
+        this.links = links;
+        this.year = year;
+        this.originalLanguage = originalLanguage;
+        this.status = status;
+        this.connector = connector;
+        this.downloadUrl = downloadUrl;
     }
 }
