7aeb6bcc2f75f7ebc614226ce1289d2fbb182804
glax <--local>
2023-05-18 15:49:08 +0200

    Added executable for testing.

---
+++ Tranga-CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tranga;
+using Tranga.Connectors;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MangaDex mangaDexConnector = new MangaDex();
+        Publication[] publications = mangaDexConnector.GetPublications();
+        Console.ReadKey();
+    }
+
+}
