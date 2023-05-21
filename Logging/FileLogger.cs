e24652b83e3ad01a4936ac03bb74d1f4da2df05c
glax <johanna@bernloehr.eu>
2023-05-21 15:33:01 +0200

    Added logfile-count limit

--- Logging/FileLogger.cs
+++ Logging/FileLogger.cs
@@ -6,10 +6,16 @@ namespace Logging;
 public class FileLogger : LoggerBase
 {
     private string logFilePath { get; }
+    private const int MaxNumberOfLogFiles = 5;
 
     public FileLogger(string logFilePath, TextWriter? stdOut, Encoding? encoding = null) : base (stdOut, encoding)
     {
         this.logFilePath = logFilePath;
+        
+        //Remove oldest logfile if more than MaxNumberOfLogFiles
+        string parentFolderPath = Path.GetDirectoryName(logFilePath)!;
+        for (int fileCount = new DirectoryInfo(parentFolderPath).EnumerateFiles().Count(); fileCount > MaxNumberOfLogFiles - 1; fileCount--) //-1 because we create own logfile later
+            File.Delete(new DirectoryInfo(parentFolderPath).EnumerateFiles().MinBy(file => file.LastWriteTime)!.FullName);
     }
     
     protected override void Write(LogMessage logMessage)
