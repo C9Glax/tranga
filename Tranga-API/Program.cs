53bff61174eeb7aaa8c67a5ca898cbf363f21152
glax <johanna@bernloehr.eu>
2023-05-21 22:01:28 +0200

    Added Swagger

--- Tranga-API/Program.cs
+++ Tranga-API/Program.cs
@@ -25,8 +25,14 @@ else
 TaskManager taskManager = new (settings, logger);
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddNewtonsoftJson();
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/GetAvailableControllers", () =>  taskManager.GetAvailableConnectors());
 
