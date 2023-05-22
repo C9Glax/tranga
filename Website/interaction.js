e2b88881305ca851f3ff56ae9d563478dfb0d908
glax <johanna@bernloehr.eu>
2023-05-22 23:52:35 +0200

    #1 Basic layout and colors

---
+++ Website/interaction.js
@@ -0,0 +1,32 @@
+﻿const slideInRight = [
+    { right: "-20rem" },
+    { right: "0" }
+];
+
+const slideInRightTiming = {
+    duration: 200,
+    iterations: 1,
+    fill: "forwards",
+    easing: "ease-out"
+}
+
+const slideOutRightTiming = {
+    direction: "reverse",
+    duration: 200,
+    iterations: 1,
+    fill: "forwards",
+    easing: "ease-in"
+}
+
+const settingsTab = document.querySelector("#settingstab");
+const settingsCog = document.querySelector("#settingscog");
+var slideIn = true;
+function slide(){
+    if(slideIn)
+        settingsTab.animate(slideInRight, slideInRightTiming);
+    else
+        settingsTab.animate(slideInRight, slideOutRightTiming);
+    slideIn = !slideIn;
+}
+
+settingsCog.addEventListener("click", () => slide());
