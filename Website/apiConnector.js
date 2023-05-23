290819de09c0bb1620b0b71a3fd73e5e745220f3
glax <johanna@bernloehr.eu>
2023-05-23 13:15:29 +0200

    Created first api-calls #1

--- Website/apiConnector.js
+++ Website/apiConnector.js
@@ -1,8 +1,35 @@
 ﻿const apiUri = "http://localhost:5177";
 
+function GetAvailableControllers(){
+    var uri = apiUri + "/GetAvailableControllers";
+    const response = await fetch(uri, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).catch(error => console.error('Unable to get items.', error));
+    return await response.json();
+}
+
+
 function GetTasks(){
-    var getTaskUri = apiUri + "/Tasks/GetList";
-    fetch(getTaskUri)
-        .then(response => response.json())
-        .catch(error => console.error('Unable to get items.', error));
+    var uri = apiUri + "/Tasks/GetList";
+    const response = await fetch(uri, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).catch(error => console.error('Unable to get items.', error));
+    return await response.json();
+}
+
+async function GetSettings(){
+    var uri = apiUri + "/Settings/Get";
+    const response = await fetch(uri, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).catch(error => console.error('Unable to get items.', error));
+    return await response.json();
 }
