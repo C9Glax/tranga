87d2357b41e874bfda2c860852dfa1164df4ef9b
glax <johanna@bernloehr.eu>
2023-05-23 12:51:21 +0200

    CORS Error

---
+++ Website/apiConnector.js
@@ -0,0 +1,8 @@
+﻿const apiUri = "http://localhost:5177";
+
+function GetTasks(){
+    var getTaskUri = apiUri + "/Tasks/GetList";
+    fetch(getTaskUri)
+        .then(response => response.json())
+        .catch(error => console.error('Unable to get items.', error));
+}
