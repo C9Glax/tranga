f9f802155d0f33d356bcc603b8ebccc57d4a01d2
glax <johanna@bernloehr.eu>
2023-05-26 14:43:47 +0200

    #28 why was there a '!'

--- Tranga/Connectors/MangaDex.cs
+++ Tranga/Connectors/MangaDex.cs
@@ -284,7 +284,7 @@ public class MangaDex : Connector
 
         string fileInCache = Path.Join(settings.coverImageCache, publication.coverFileNameInCache);
         string newFilePath = Path.Join(publicationFolder, "cover.", Path.GetFileName(fileInCache).Split('.')[^1]);
-        if(!File.Exists(fileInCache) && !File.Exists(newFilePath))
+        if(File.Exists(fileInCache) && !File.Exists(newFilePath))
             File.Copy(fileInCache, newFilePath);
         logger?.WriteLine(this.GetType().ToString(), $"Done cloning cover {publication.sortName}");
         
