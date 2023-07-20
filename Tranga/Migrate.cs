77eec0f69690891a03b9045c27663207e215bca4
glax <johanna@bernloehr.eu>
2023-07-21 00:32:18 +0200

    Fix wrong deserialization

--- Tranga/Migrate.cs
+++ Tranga/Migrate.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Logging;
+using Newtonsoft.Json;
 
 namespace Tranga;
 
@@ -21,7 +22,11 @@ public static class Migrate
 
     private static void RemoveUpdateLibraryTask(TrangaSettings settings)
     {
-        List<TrangaTask> tasks = JsonConvert.DeserializeObject<List<TrangaTask>>(settings.tasksFilePath)!;
+        if (!File.Exists(settings.tasksFilePath))
+            return;
+
+        string tasksJsonString = File.ReadAllText(settings.tasksFilePath);
+        List<TrangaTask> tasks = JsonConvert.DeserializeObject<List<TrangaTask>>(tasksJsonString, new JsonSerializerSettings { Converters = { new TrangaTask.TrangaTaskJsonConverter() } })!;
         tasks.RemoveAll(t => t.task == TrangaTask.Task.UpdateLibraries);
     }
 }
