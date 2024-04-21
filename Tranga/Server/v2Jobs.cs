6d48a100ca5b3cc3f44c7ba8b05c1338c4ee0ef3
Glax <johanna@bernloehr.eu>
2024-04-21 21:48:11 +0200

    Implemented GET /v2/Jobs /v2/Jobs/Running /v2/Jobs/Waiting /v2/Jobs/Monitoring /v2/Job/<jobId> /v2/Job/<jobId>/Progress

--- Tranga/Server/v2Jobs.cs
+++ Tranga/Server/v2Jobs.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Text.RegularExpressions;
+using Tranga.Jobs;
 
 namespace Tranga.Server;
 
@@ -7,22 +8,28 @@ public partial class Server
 {
     private ValueTuple<HttpStatusCode, object?> GetV2Jobs(GroupCollection groups, Dictionary<string, string> requestParameters)
     {
-        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.NotImplemented, "Not Implemented");
+        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.OK, _parent.jobBoss.jobs.Select(job => job.id));
     }
     
     private ValueTuple<HttpStatusCode, object?> GetV2JobsRunning(GroupCollection groups, Dictionary<string, string> requestParameters)
     {
-        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.NotImplemented, "Not Implemented");
+        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.OK, _parent.jobBoss.jobs
+            .Where(job => job.progressToken.state is ProgressToken.State.Running)
+            .Select(job => job.id));
     }
     
     private ValueTuple<HttpStatusCode, object?> GetV2JobsWaiting(GroupCollection groups, Dictionary<string, string> requestParameters)
     {
-        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.NotImplemented, "Not Implemented");
+        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.OK, _parent.jobBoss.jobs
+            .Where(job => job.progressToken.state is ProgressToken.State.Waiting)
+            .Select(job => job.id));
     }
     
     private ValueTuple<HttpStatusCode, object?> GetV2JobsMonitoring(GroupCollection groups, Dictionary<string, string> requestParameters)
     {
-        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.NotImplemented, "Not Implemented");
+        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.OK, _parent.jobBoss.jobs
+            .Where(job => job.jobType is Job.JobType.DownloadNewChaptersJob)
+            .Select(job => job.id));
     }
     
     private ValueTuple<HttpStatusCode, object?> PostV2JobsCreateMonitorInternalId(GroupCollection groups, Dictionary<string, string> requestParameters)
@@ -47,7 +54,13 @@ public partial class Server
     
     private ValueTuple<HttpStatusCode, object?> GetV2JobJobId(GroupCollection groups, Dictionary<string, string> requestParameters)
     {
-        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.NotImplemented, "Not Implemented");
+        if (groups.Count < 1 ||
+            !_parent.jobBoss.TryGetJobById(groups[1].Value, out Job? job) ||
+            job is null)
+        {
+            return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.BadRequest, $"Job with ID: '{groups[1].Value}' does not exist.");
+        }
+        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.OK, job);
     }
     
     private ValueTuple<HttpStatusCode, object?> DeleteV2JobJobId(GroupCollection groups, Dictionary<string, string> requestParameters)
@@ -57,7 +70,14 @@ public partial class Server
     
     private ValueTuple<HttpStatusCode, object?> GetV2JobJobIdProgress(GroupCollection groups, Dictionary<string, string> requestParameters)
     {
-        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.NotImplemented, "Not Implemented");
+        
+        if (groups.Count < 1 ||
+            !_parent.jobBoss.TryGetJobById(groups[1].Value, out Job? job) ||
+            job is null)
+        {
+            return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.BadRequest, $"Job with ID: '{groups[1].Value}' does not exist.");
+        }
+        return new ValueTuple<HttpStatusCode, object?>(HttpStatusCode.OK, job.progressToken);
     }
     
     private ValueTuple<HttpStatusCode, object?> PostV2JobJobIdStartNow(GroupCollection groups, Dictionary<string, string> requestParameters)
