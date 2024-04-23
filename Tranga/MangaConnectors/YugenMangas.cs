a9147f5808f93e8f9d0e796fb8210da0f5192b95
William Brockhus <=>
2024-04-24 08:43:01 +1000

    chore: remove deprecated site

--- Tranga/MangaConnectors/YugenMangas.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tranga.MangaConnectors;
-
-public class YugenMangas : HeanCms
-{
-    protected override string hostname => "https://api.yugenmangas.net";
-    public YugenMangas (GlobalBase clone) : base(clone, "YugenMangas")
-    {
-    }
-}
