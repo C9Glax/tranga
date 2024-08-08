bc101363319836aa9b3564c6b78c070b20f81b45
Glax <johanna@bernloehr.eu>
2024-08-08 21:00:37 +0200

    MangaHere image download sucks, you have to iterate all over all images one by one. Have some extra traffic then, idc.
    https://github.com/C9Glax/tranga/issues/69

--- Tranga/Tranga.cs
+++ Tranga/Tranga.cs
@@ -25,7 +25,8 @@ public partial class Tranga : GlobalBase
             new Mangaworld(this),
             new Bato(this),
             new MangaLife(this),
-            new ManhuaPlus(this)
+            new ManhuaPlus(this),
+            new MangaHere(this),
         };
         foreach(DirectoryInfo dir in new DirectoryInfo(Path.GetTempPath()).GetDirectories("trangatemp"))//Cleanup old temp folders
             dir.Delete();
