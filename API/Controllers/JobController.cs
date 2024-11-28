2d8d6fa0fc6546b3a20d86f28ca0a70d35ff738b
Glax <johanna@bernloehr.eu>
2024-11-28 21:22:11 +0100

    JobController

--- API/Controllers/JobController.cs
+++ API/Controllers/JobController.cs
@@ -2,6 +2,7 @@
 using API.Schema.Jobs;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
+using static Microsoft.AspNetCore.Http.StatusCodes;
 
 namespace API.Controllers;
 
@@ -11,57 +12,144 @@ namespace API.Controllers;
 public class JobController(PgsqlContext context) : Controller
 {
     [HttpGet]
+    [ProducesResponseType<Job[]>(Status200OK)]
     public IActionResult GetJobs([FromBody] string[] ids)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        Job[] ret = context.Jobs.Where(job => ids.Contains(job.JobId)).ToArray();
+        return Ok(ret);
     }
     
     [HttpGet("Types")]
+    [ProducesResponseType<string[]>(Status200OK)]
     public IActionResult GetJobTypes()
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        return Ok(Enum.GetNames<JobType>());
+    }
+
+    [HttpGet("Due")]
+    [ProducesResponseType<Job[]>(Status200OK)]
+    public IActionResult GetDueJobs()
+    {
+        DateTime now = DateTime.Now;
+        Job[] dueJobs = context.Jobs.Where(job => now < job.NextExecution).ToArray();
+        return Ok(dueJobs);
     }
     
     [HttpGet("States")]
+    [ProducesResponseType<string[]>(Status200OK)]
     public IActionResult GetJobStates()
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        return Ok(Enum.GetNames<JobState>());
     }
 
-    [HttpGet("State/{state}")]
-    public IActionResult GetJobsInState(string state)
+    [HttpGet("State/{stateStr}")]
+    [ProducesResponseType<Job[]>(Status200OK)]
+    [ProducesResponseType<Exception>(Status400BadRequest)]
+    public IActionResult GetJobsInState(string stateStr)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        try
+        {
+            JobState state = Enum.Parse<JobState>(stateStr);
+            Job[] jobsInState = context.Jobs.Where(job => job.state == state).ToArray();
+            return Ok(jobsInState);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
     }
 
     [HttpGet("Type/{type}")]
-    public IActionResult GetJobsOfType(string type)
+    [ProducesResponseType<Job[]>(Status200OK)]
+    [ProducesResponseType<Exception>(Status400BadRequest)]
+    public IActionResult GetJobsOfType(string typeStr)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        try
+        {
+            JobType type = Enum.Parse<JobType>(typeStr);
+            Job[] jobsOfType = context.Jobs.Where(job => job.JobType == type).ToArray();
+            return Ok(jobsOfType);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType<Job>(Status200OK)]
+    [ProducesResponseType(Status404NotFound)]
     public IActionResult GetJob(string id)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        Job? ret = context.Jobs.Find(id);
+        return (ret is not null) switch
+        {
+            true => Ok(ret),
+            false => NotFound()
+        };
     }
 
     [HttpPost("Create")]
+    [ProducesResponseType(Status201Created)]
+    [ProducesResponseType(Status500InternalServerError)]
     public IActionResult CreateJob([FromBody]Job job)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        try
+        {
+            context.Jobs.Add(job);
+            context.SaveChanges();
+            return Created();
+        }
+        catch (Exception e)
+        {
+            return new StatusCodeResult(500);
+        }
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(Status200OK)]
+    [ProducesResponseType(Status404NotFound)]
+    [ProducesResponseType(Status500InternalServerError)]
     public IActionResult DeleteJob(string id)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        try
+        {
+            Job? ret = context.Jobs.Find(id);
+            switch (ret is not null)
+            {
+                case true:
+                    context.Remove(ret);
+                    context.SaveChanges();
+                    return Ok();
+                case false: return NotFound();
+            }
+        }
+        catch (Exception e)
+        {
+            return new StatusCodeResult(500);
+        }
     }
 
     [HttpPost("{id}/Start")]
+    [ProducesResponseType(Status202Accepted)]
+    [ProducesResponseType(Status404NotFound)]
+    [ProducesResponseType(Status500InternalServerError)]
     public IActionResult StartJob(string id)
     {
-        return NotFound(new ProblemResponse("Not implemented"));
+        Job? ret = context.Jobs.Find(id);
+        if (ret is null)
+            return NotFound();
+        ret.NextExecution = DateTime.UnixEpoch;
+        try
+        {
+            context.Update(ret);
+            context.SaveChanges();
+            return Accepted();
+        }
+        catch (Exception e)
+        {
+            return new StatusCodeResult(500);
+        }
     }
 
     [HttpPost("{id}/Stop")]
