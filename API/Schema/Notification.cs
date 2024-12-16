3ee3a07565dbd12e7f24da245baad0e48d9f08e3
Glax <johanna@bernloehr.eu>
2024-12-16 17:58:25 +0100

    Urgency

--- API/Schema/Notification.cs
+++ API/Schema/Notification.cs
@@ -4,21 +4,14 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Schema;
 
 [PrimaryKey("AltTitleId")]
-public class Notification(string title, string message = "", Notification.NotificationUrgency notificationUrgency = Notification.NotificationUrgency.Normal)
+public class Notification(string title, string message = "", byte urgency = 2)
 {
     [MaxLength(64)]
     public string NotificationId { get; init; } = TokenGen.CreateToken("Notification", 64);
 
-    public NotificationUrgency Urgency { get; init; } = notificationUrgency;
+    public byte Urgency { get; init; } = urgency;
 
     public string Title { get; init; } = title;
 
     public string Message { get; init; } = message;
-
-    public enum NotificationUrgency : byte
-    {
-        Low = 0,
-        Normal = 1,
-        High = 3
-    }
 }
