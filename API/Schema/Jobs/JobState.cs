8a0829ef692b920273c3b93601935e28dde04702
Glax <johanna@bernloehr.eu>
2025-03-03 21:11:39 +0100

    JobState set enum to bytes with fixed values

--- API/Schema/Jobs/JobState.cs
+++ API/Schema/Jobs/JobState.cs
@@ -1,8 +1,13 @@
 ﻿namespace API.Schema.Jobs;
 
-public enum JobState
+public enum JobState : byte
 {
-    Waiting,
-    Running,
-    Completed
+    //Values 0-63 Preparation Stages
+    Waiting = 0, 
+    //64-127 Running Stages
+    Running = 64,
+    //128-191 Completion Stages
+    Completed = 128,
+    //192-255 Error stages
+    Failed = 192
 }
