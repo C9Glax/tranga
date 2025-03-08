c3ebd6acac991d3ccd27bcb5558870fdf76e40f1
Glax <johanna@bernloehr.eu>
2025-03-08 13:10:39 +0100

    Database Migrations

--- API/Migrations/20250308121028_dev-080325-1.Designer.cs
+++ API/Migrations/20250308121028_dev-080325-1.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using API.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace API.Migrations
 {
     [DbContext(typeof(PgsqlContext))]
-    partial class PgsqlContextModelSnapshot : ModelSnapshot
+    [Migration("20250308121028_dev-080325-1")]
+    partial class dev0803251
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
