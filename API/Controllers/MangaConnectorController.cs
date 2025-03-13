0df3381c8c4273261bdcf9ed0ffc020a9062a945
Glax <johanna@bernloehr.eu>
2025-03-13 17:29:17 +0100

    Fix Base-Endpoint-Path for MangaConnectorController.cs

--- API/Controllers/MangaConnectorController.cs
+++ API/Controllers/MangaConnectorController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers;
 
 [ApiVersion(2)]
 [ApiController]
-[Route("v{v:apiVersion}")]
+[Route("v{v:apiVersion}/[controller]")]
 public class MangaConnectorController(PgsqlContext context) : Controller
 {
     /// <summary>
