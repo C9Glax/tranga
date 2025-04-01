c696c38983b6d30b81cbd420489f31713ab86ca3
Glax <johanna@bernloehr.eu>
2025-04-01 02:14:46 +0200

    Fix Weebcentral

---
+++ API/Migrations/20250401001439_dev-010425-1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class dev0104251 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalLanguage",
+                table: "Mangas",
+                type: "character varying(8)",
+                maxLength: 8,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8)",
+                oldMaxLength: 8);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalLanguage",
+                table: "Mangas",
+                type: "character varying(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(8)",
+                oldMaxLength: 8,
+                oldNullable: true);
+        }
+    }
+}
