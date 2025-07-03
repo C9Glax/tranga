fd508b7e6ca674a5e4b0d13ed39aad299f261c3f
glax <johanna@bernloehr.eu>
2025-07-03 20:38:29 +0200

    UpdateMetadataWorker.cs

---
+++ API/Workers/PeriodicWorkers/UpdateMetadataWorker.cs
@@ -0,0 +1,31 @@
+using API.Schema.MangaContext;
+using API.Schema.MangaContext.MetadataFetchers;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Workers;
+
+public class UpdateMetadataWorker(TimeSpan? interval = null, IEnumerable<BaseWorker>? dependsOn = null)
+    : BaseWorkerWithContext<MangaContext>(dependsOn), IPeriodic
+{
+
+    public DateTime LastExecution { get; set; } = DateTime.UtcNow;
+    public TimeSpan Interval { get; set; } = interval ?? TimeSpan.FromHours(12);
+    
+    protected override BaseWorker[] DoWorkInternal()
+    {
+        IQueryable<string> mangaIds = DbContext.MangaConnectorToManga
+            .Where(m => m.UseForDownload)
+            .Select(m => m.ObjId);
+        IQueryable<MetadataEntry> metadataEntriesToUpdate = DbContext.MetadataEntries
+            .Include(e => e.MetadataFetcher)
+            .Where(e =>
+                mangaIds.Any(id => id == e.MangaId));
+        
+        foreach (MetadataEntry metadataEntry in metadataEntriesToUpdate)
+            metadataEntry.MetadataFetcher.UpdateMetadata(metadataEntry, DbContext);
+
+        DbContext.Sync();
+
+        return [];
+    }
+}
