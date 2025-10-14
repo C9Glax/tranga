352c4a4401208b12a081df6d48a08939e4ae731f
glax <johanna@bernloehr.eu>
2025-10-14 13:29:33 +0200

    MetadataSearchResult data annotations

--- API/Schema/MangaContext/MetadataFetchers/MetadataSearchResult.cs
+++ API/Schema/MangaContext/MetadataFetchers/MetadataSearchResult.cs
@@ -1,3 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Schema.MangaContext.MetadataFetchers;
 
-public record MetadataSearchResult(string Identifier, string Name, string Url, string? Description = null, string? CoverUrl = null);
+public record MetadataSearchResult(
+    string Identifier,
+    string Name,
+    string Url,
+    string? Description = null,
+    string? CoverUrl = null)
+{
+    /// <summary>
+    /// Identifier specific to the MetadataFetcher
+    /// </summary>
+    [Required]
+    public string Identifier { get; init; } = Identifier;
+    /// <summary>
+    /// Name of the Manga
+    /// </summary>
+    [Required]
+    public string Name { get; init; } = Name;
+    /// <summary>
+    /// Url to the result
+    /// </summary>
+    [Required]
+    public string Url { get; init; } = Url;
+    /// <summary>
+    /// Description of the Manga
+    /// </summary>
+    [Required]
+    public string? Description { get; init; } = Description;
+    /// <summary>
+    /// Url to the cover if available
+    /// </summary>
+    [Required]
+    public string? CoverUrl { get; init; } = CoverUrl;
+}
