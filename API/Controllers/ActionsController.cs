1b881fb7eb136b265212d540907a235452c28854
glax <johanna@bernloehr.eu>
2025-10-20 19:43:08 +0200

    Pagination request value check

--- API/Controllers/ActionsController.cs
+++ API/Controllers/ActionsController.cs
@@ -35,12 +35,16 @@ public class ActionsController(ActionsContext context) : Controller
     /// <param name="page">Page to request (default 1)</param>
     /// <param name="pageSize">Size of Page (default 10)</param>
     /// <response code="200">List of performed actions</response>
+    /// <response code="400">Page data wrong</response>
     /// <response code="500">Database error</response>
     [HttpPost("Filter")]
     [ProducesResponseType<PagedResponse<ActionRecord>>(Status200OK, "application/json")]
     [ProducesResponseType(Status500InternalServerError)]
-    public async Task<Results<Ok<PagedResponse<ActionRecord>>, InternalServerError>> GetActionsInterval([FromBody]Filter filter, [FromQuery]int page = 1, [FromQuery]int pageSize = 10)
+    [ProducesResponseType(Status400BadRequest)]
+    public async Task<Results<Ok<PagedResponse<ActionRecord>>, BadRequest, InternalServerError>> GetActionsInterval([FromBody]Filter filter, [FromQuery]int page = 1, [FromQuery]int pageSize = 10)
     {
+        if (page < 1 || pageSize < 1)
+            return TypedResults.BadRequest();
         if (await context.FilterActions(filter.MangaId, filter.ChapterId)
                 .Where(a => filter.Start == null || a.PerformedAt >= filter.Start.Value.ToUniversalTime())
                 .Where(a => filter.End == null || a.PerformedAt <= filter.End.Value.ToUniversalTime())
