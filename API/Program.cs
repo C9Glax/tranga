6e29ba3e36f814d2f06a0ddfedf09abfb3e385c6
Glax <johanna@bernloehr.eu>
2026-03-29 07:32:32 +0200

    Fix OpenApi Doc generation

--- API/Program.cs
+++ API/Program.cs
@@ -2,16 +2,12 @@ using System.Text.Json.Serialization;
 using API;
 using Database;
 using Database.MangaContext;
-using Microsoft.OpenApi;
 using Npgsql;
 using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi(opts =>
-{
-    opts.OpenApiVersion = OpenApiSpecVersion.OpenApi3_1;
-}).ConfigureHttpJsonOptions(opts =>
+builder.Services.AddOpenApi().ConfigureHttpJsonOptions(opts =>
 {
     opts.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
@@ -20,21 +16,21 @@ builder.Services.AddDbContext<MangaContext>(opts => opts.Configure(null), Servic
 
 WebApplication app = builder.Build();
 
+RouteGroupBuilder routeBuilder = app.MapGroup("/");
+Endpoints.AddEndpoints(routeBuilder);
+
+app.UseHttpsRedirection();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
 try
 {
     await app.Services.CreateScope().ServiceProvider.GetRequiredService<MangaContext>().ApplyMigrations();
-
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-
-    RouteGroupBuilder routeBuilder = app.MapGroup("/");
-    Endpoints.AddEndpoints(routeBuilder);
-
-    app.UseHttpsRedirection();
-
-    app.Run();
 }
 catch (NpgsqlException)
 {
     // probably build
 }
+    
+app.Run();
