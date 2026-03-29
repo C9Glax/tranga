cbf3a3a4b1b747b1e91bc6d1c44394ad705c3ea8
Glax <johanna@bernloehr.eu>
2026-03-29 08:17:44 +0200

    Add "unmonitored" query to GetListMangaEndpoint.cs

--- API/Features/Manga/GetListMangaEndpoint.cs
+++ API/Features/Manga/GetListMangaEndpoint.cs
@@ -18,15 +18,17 @@ public abstract class GetListMangaEndpoint
     /// </summary>
     /// <param name="mangaContext"></param>
     /// <param name="includes">Additional Information to include</param>
+    /// <param name="unmonitored">All known Manga</param>
     /// <param name="ct"></param>
     /// <returns>List of Manga</returns>
     /// <response code="200">List of Manga</response>
-    public static async Task<Results<Ok<MangaDTO[]>, InternalServerError>> Handle(MangaContext mangaContext, [FromQuery]GetMangaEndpoint.Includes[] includes, CancellationToken ct)
+    public static async Task<Results<Ok<MangaDTO[]>, InternalServerError>> Handle(MangaContext mangaContext, [FromQuery]GetMangaEndpoint.Includes[] includes, [FromQuery]bool unmonitored, CancellationToken ct)
     {
         // TODO Pagination
         if (await mangaContext.Mangas
                 .IncludeDownloadLinks(includes.Contains(GetMangaEndpoint.Includes.DownloadLinks))
                 .IncludeMetadataLinks(includes.Contains(GetMangaEndpoint.Includes.MetadataLinks))
+                .Where(m => unmonitored || m.Monitor)
                 .ToListAsync(ct) is not { } mangas)
             return TypedResults.InternalServerError();
         
