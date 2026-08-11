ca9779ae4413b107763ef6bd14c6cee86bb37b58
Glax <its@glax.dev>
2026-08-12 00:09:43 +0200

    Fix cover sizing

--- Frontend/app/components/Manga/List.vue
+++ Frontend/app/components/Manga/List.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="flex flex-row flex-wrap gap-2">
-        <USkeleton v-for="_ in [...Array(3)]" v-if="loading" class="w-60 h-90" />
-
-        <UPageCard
-            v-for="manga in mangas"
-            v-if="!loading"
-            v-bind="selectMode ? {} : { to: `/manga/${manga.mangaId}` }"
-            class="relative overflow-clip"
-            :class="selectMode && 'cursor-pointer'"
-            :ui="{ container: 'p-0 sm:p-0' }"
-            @click="selectMode ? $emit('select', manga) : useOverlay().closeAll()">
-            <p class="z-1 absolute text-2xl mx-2 my-3 font-bold text-shadow-sm">
-                <TrangaTitle :title="manga.metadataEntry?.series" />
-                <UBadge v-if="manga?.metadataEntry?.nsfw" label="NSFW" color="error" variant="solid" />
-            </p>
-            <MangaCover
-                :file-id="manga.metadataEntry?.coverId"
-                :manga-id="manga.mangaId"
-                no-blur
-                class="z-0"
-                :class="manga.metadataEntry?.nsfw ? 'blur-md' : 'blur-xs'" />
-        </UPageCard>
+    <div class="flex flex-row flex-wrap gap-2 justify-center md:justify-start">
+        <div v-if="loading">
+            <USkeleton v-for="k in [...Array(3)]" :key="k" class="w-60 h-90" />
+        </div>
+        <div v-else>
+            <UPageCard
+                v-for="manga in mangas"
+                :key="manga.mangaId"
+                v-bind="selectMode ? {} : { to: `/manga/${manga.mangaId}` }"
+                class="relative overflow-clip max-w-90 aspect-6/9"
+                :class="selectMode && 'cursor-pointer'"
+                :ui="{ container: 'p-0 sm:p-0' }"
+                @click="selectMode ? $emit('select', manga) : useOverlay().closeAll()">
+                <p class="z-1 absolute text-2xl mx-2 my-3 font-bold text-shadow-sm">
+                    <TrangaTitle :title="manga.metadataEntry?.series" />
+                    <UBadge v-if="manga?.metadataEntry?.nsfw" label="NSFW" color="error" variant="solid" />
+                </p>
+                <MangaCover
+                    :file-id="manga.metadataEntry?.coverId"
+                    :manga-id="manga.mangaId"
+                    no-blur
+                    class="z-0"
+                    :class="manga.metadataEntry?.nsfw ? 'blur-md' : 'blur-xs'" />
+            </UPageCard>
+        </div>
 
         <div v-if="(mangas?.length ?? 0) < 1 && !loading" class="w-max flex gap-2">
             <UIcon name="i-lucide-brackets" class="size-15" />
