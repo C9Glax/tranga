d92cb31eaa7d5fa0b81966492896a37acd4d117c
Glax <its@glax.dev>
2026-08-13 23:31:03 +0200

    Display library name as link instead of url

--- Frontend/app/pages/index.vue
+++ Frontend/app/pages/index.vue
@@ -30,7 +30,7 @@ const navigation = computed((): TrangaPageTitleProps | undefined =>
               title: { label: 'Libraries', type: 'label' },
               items:
                   libraries.value?.map((library) => ({
-                      label: library.baseUrl,
+                      label: library.id,
                       to: library.baseUrl,
                       external: true,
                       icon: 'i-lucide-library',
